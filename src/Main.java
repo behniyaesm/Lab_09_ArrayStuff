d904ee1ce3a79d88eb6e6e19e60a16aa5b232569
behniyaesm <behniyaesm@gmail.com>
2024-11-04 17:15:30 -0500

    done

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import java.util.Random;
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
         Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
         int[] dataPoints = new int[100];
         int sum = 0;
         double average = 0;
@@ -28,6 +31,53 @@ public class Main
         System.out.println("The sum of the random array dataPoints is: " + sum);
         System.out.println("The average of the random array dataPoints is: " + average);
 
-        
+        //get number from user
+        int userInput = SafeInput.getRangedInt(sc,"Enter a number between", 1, 100);
+        System.out.println("You entered " + userInput);
+
+        int count = 0;
+        for (int i = 0; i < dataPoints.length; i++) {
+            if (dataPoints[i] == userInput) {
+                count++;
+            }
+        }
+
+        System.out.println("The value " + userInput + " was found " + count + " times in the array.");
+
+        //find user number in the array
+        boolean done = false;
+        for (int i = 0; i < dataPoints.length; i++) {
+            if (dataPoints[i] == userInput) {
+                System.out.println("The value " + userInput + " was found at array index " + i + ".");
+                done = true;
+                break;
+            }
+        }
+        if (!done) {
+            System.out.println("The value " + userInput + " was not found in the array.");
+        }
+
+        int min = dataPoints[0];
+        int max = dataPoints[0];
+
+        for (int i = 1; i < dataPoints.length; i++) {
+            if (dataPoints[i] < min) {
+                min = dataPoints[i];
+            } else if (dataPoints[i] > max) {
+                max = dataPoints[i];
+            }
+        }
+
+        System.out.println("The minimum value in the array is: " + min);
+        System.out.println("The maximum value in the array is: " + max);
+
+        System.out.println("Average of dataPoints is: " + getAverage(dataPoints));
+    }
+    public static double getAverage(int values[]) {
+        int sum = 0;
+        for (int value : values) {
+            sum += value;
+        }
+        return (double) sum / values.length;
     }
 }
